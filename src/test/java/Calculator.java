d7c0f273a1aac0a1b9aa2ebbd48e681d2e24138b
Saleh Basalim <sbasalim@una.edu>
2024-11-15 01:25:36 -0600

    update

---
+++ src/test/java/Calculator.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    @Test
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        assertEquals(5, calculator.add(2, 3));
+    }
+}
