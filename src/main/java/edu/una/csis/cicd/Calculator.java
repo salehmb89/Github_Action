f4c22d31b1baf07d4e4fecf7da31258a79c03a84
Saleh Basalim <156353463+salehmb89@users.noreply.github.com>
2024-11-16 01:24:44 -0600

    Update Calculator.java

--- src/main/java/edu/una/csis/cicd/Calculator.java
+++ src/main/java/edu/una/csis/cicd/Calculator.java
@@ -1,6 +1,15 @@
 package edu.una.csis.cicd;
+
+import java.util.logging.Logger;
+
 public class Calculator {
     public int add(int a, int b) {
         return a + b;
+    }
+    
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(2, 3);
+        System.out.println("2 + 3 = " + result);
     }
 }
